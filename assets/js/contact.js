4fcd9879e3e9a1ab8e6fbaf9b157e1c3213e4500
davidyge <dgonzalesespinoza2.20@gmail.com>
2024-09-24 23:43:05 -0500

    Actualizacion de formulario de envio

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,23 +1,35 @@
-document.getElementById('loading').style.display = 'none';
-document.getElementById('sent-mess2').style.color = 'red';
-document.getElementById('error-message').style.display = 'block';
+
+let send = document.getElementById('sent-mess');
+let error =document.getElementById('err-mess');
+
+// Oculta inicialmente los mensajes de éxito y error
+send.style.display = 'none';
+error.style.display = 'none';
+
 const btn = document.getElementById('button');
+const form = document.getElementById('form');
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
+ form.addEventListener('submit', function(event) {
    event.preventDefault();
-
-   btn.value = 'Sending...';
-
+   btn.value = 'Sending...'; // Cambia el texto del botón mientras envía
    const serviceID = 'service_196494s';
    const templateID = 'template_po03yhg';
-
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Send Email';
-      alert('Sent!');
+      send.style.display = 'block';
+      // Oculta el mensaje de éxito después de 5 segundos (5000 milisegundos)
+      setTimeout(() => {
+        send.style.display = 'none';
+      }, 5000);
+
+      form.reset();
     }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+      error.style.display = 'block';
+      setTimeout(() => {
+        error.style.display = 'none';
+      }, 5000);
+     // alert(JSON.stringify(err));
+     //  errorMessage.textContent = `Error: ${JSON.stringify(err)}`;  // Muestra el error en el mensaje
     });
 });
